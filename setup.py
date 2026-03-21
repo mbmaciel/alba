1a373b924382c3d4aa5b0261eb44290d6a80408d
mbmaciel <mbmaciel@outlook.com>
2026-03-21 14:22:17 -0300

    banco retirado do build

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from cx_Freeze import Executable, setup
 # cxfreeze.exe .\main.py --target-dir .\dist\ --base-name=gui
 build_exe_options = {
     "build_exe": "dist",
-    "include_files": [
-        ("database/alba.db", "database/alba.db"),
-    ],
+    #"include_files": [
+    #    ("database/alba.db", "database/alba.db"),
+    #],
 }
 
 setup(
